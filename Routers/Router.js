886abec60d1c7bf6850748cb646ca734d505644a
Dilip Romi <dilip.romi@ssil.com>
2022-09-21 17:13:49 +0530

    using lia

--- Routers/Router.js
+++ Routers/Router.js
@@ -1,7 +1,7 @@
 const router=require('express').Router()
 const validateDto = require('../middleware/Validation');
-const userSchema = require('../schema/user');
+const userSchema = require('../schema/customer_entity');
 
-router.post('/register', validateDto(userSchema), userController.register);
+// router.post('/register', validateDto(userSchema), userController.register);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express=require('express');
 const app = express();
+const cors = require('cors');
+const compression=require('compression')
+
+const morgan = require("morgan")
 const dbConnection=require('./Config/dbConnection')
+const Router=require('./Routers/Router')
+const bodyParser=require("body-parser");
+
 require('dotenv').config()
 
+app.use(compression({
+    level:6,
+    threshold:10*1000,
+    filter:(req,res)=>{
+        if(req.headers['x-no-compression']){
+            return false
+        }
+        return compression.filter(req,res)
+    }
+}))
 
-
-app.listen(80,()=>{
-    console.log('listening on port 80');
+const PORT=process.env.SERVER_PORT
+app.listen(PORT,()=>{
+    console.log(`Server runing http://localhost:${PORT}`);
 })
